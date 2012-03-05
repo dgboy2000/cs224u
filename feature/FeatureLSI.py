a9de4be0f048137d5d732d4b97e4fd6f5a61a467
Dan Preston <dan.r.preston@gmail.com>
2012-03-04 23:06:06 -0800

    Using tfidf instead of BoW

---
+++ feature/FeatureLSI.py
@@ -0,0 +1,46 @@
+import abc
+from FeatureBase import FeatureBase
+import nltk
+from nltk.collocations import *
+import Corpus
+import os
+import cPickle as pickle
+import params
+import numpy as np
+import LanguageUtils
+
+class FeatureLSI(object):
+
+    def __init__(self):
+        self.features = list()
+        self.type = 'undefined'
+
+    def numFeatures(self):
+        """Return the number of features for this feature vector"""
+        return length(self.features)
+
+    def featureType(self):
+        """Returns string description of the feature type, such as 'real-valued', 'binary', 'enum', etc."""
+        return self.type
+
+    def getFeatureMatrix(self):
+        """Returns ordered list of features."""
+        return self.features
+
+    def extractFeatures(self, ds, corpus):
+        """Extracts features from a DataSet ds"""
+
+        feats = list()
+        lsi = corpus.getLSA()
+        tfidf = corpus.getTfidf()
+        for mm in ds.getGensimCorpus():
+            cur_feat = list()
+            for topic, score in lsi[tfidf[mm]]:
+                cur_feat.append(score)
+            feats.append(cur_feat)
+
+        self.features = np.asarray(feats)
+        return
+
+FeatureBase.register(FeatureLSI)
+
