b9e24683a42733d8ee4dd90592adccbe53e525a6
Danny Goodman <goodman.danny@gmail.com>
2012-04-22 16:43:13 -0700

    fix package script to create output/DUMMY

--- package_model.sh
+++ package_model.sh
@@ -1,3 +1,4 @@
+touch output/DUMMY
 tar -cvjf cs224u-kaggle.tar.bz2 *.py *.sh en_wsj.model *.aff *.dic feature/ learn/ matlab/ output/DUMMY *.m score/ spelling/ test/
 zip cs224u-kaggle.tar.bz2.zip cs224u-kaggle.tar.bz2
 
