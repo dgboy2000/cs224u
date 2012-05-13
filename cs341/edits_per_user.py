c42e4a67d86a26c5b4a1052c213d5ad2ac5c7214
Danny Goodman <goodman.danny@gmail.com>
2012-05-13 16:51:01 -0700

    mapper for counting number of edits per user

--- cs341/edits_per_user.py
+++ cs341/edits_per_user.py
@@ -1,12 +1,10 @@
 # From http://mail.python.org/pipermail/python-dev/2000-October/009946.html
 
+import re
 import sys, string
-
 from xml.sax import saxutils, handler, make_parser
 
-
 class RevisionHandler(handler.ContentHandler):
-
   def __init__(self):
       handler.ContentHandler.__init__(self)
       self._curPage = None
@@ -30,13 +28,33 @@ class RevisionHandler(handler.ContentHandler):
   def characters(self, content):
     if self._inContributorTag and self._idType is not None:
       self._revisionAuthors.append(content)
+      if len(self._revisionAuthors) % 10000 == 0:
+        print "Processed %d revisions" %len(self._revisionAuthors)
 
-# # --- The main program
-# 
-# parser = make_parser()
-# parser.setContentHandler(ContentGenerator())
-# parser.parse(sys.argv[1])
+# --- The main program
+def testXML():
+  parser = make_parser()
+  revisionHandler = RevisionHandler()
+  parser.setContentHandler(revisionHandler)
+  parser.parse("/Users/dannygoodman/Downloads/enwiki-20111201-stub-meta-history1.xml")
+  print "Processed %d total revisions" %len(revisionHandler._revisionAuthors)
+  
+  #!/usr/bin/python
+  import sys 
+  import re 
 
+def main(argv): 
+  pattern = re.compile("^        <i(?:d|p)>(\d+)</i(?:d|p)>\s*$") 
+  for line in sys.stdin: 
+    match = pattern.match(line)
+    if match is not None:
+      print "LongValueSum:" + match.group(1) + "\t" + "1" 
+
+
+
+
+if __name__ == "__main__": 
+    main(sys.argv)
 
 
 
